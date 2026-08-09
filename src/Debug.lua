126dd35971625daa2bdb09190576bddf0688082e
Lit-Laurent <cooperjallen03@gmail.com>
2026-08-09 16:58:33 -0700

    update Debug.lua, textbox positions are updated in Debug:draw() now before they draw fixing previous overlap issues, and less important polish stuff

--- src/Debug.lua
+++ src/Debug.lua
@@ -1,16 +1,28 @@
 local Textbox = require("src/Textbox")
 local Debug = {messages = {}}
 
-function Debug:newMessage(msg,id)
-	local x = #self.messages * 300
-	local textbox = Textbox:new(msg,x,0, 300, 100,true,{0.8,0.8,0.8,0.8})
+function Debug:newMessage(msg,id,boxC,textC)
+	local debugBoxColor = boxC
+		or {0.5,0.4,0.2,0.8}
+		--or {0.8,0.8,0.8,0.8} -- Light Grey
+
+	local debugTextColor = textC
+		or {0,0,0,1} -- Default black
+
+	-- x is set to nil because it is updated in draw() anyways
+	local textbox = Textbox:new(msg,nil,30, 300, 100,true,debugBoxColor,debugTextColor)
 	if id then textbox.id = id end
 	table.insert(self.messages,textbox)
 end
 
 function Debug:draw()
-	for _, msg in ipairs(self.messages) do
+	local marginL = 80
+	local msgSeparation = 50 -- This isn't proper but I got it lookin how I want
+	for i, msg in ipairs(self.messages) do
+		msg.x = (((i - 1) * msg.boxW) + marginL) + msgSeparation * i - 1
 		msg:draw()
+		love.graphics.setColor({0,0,0,1}) -- an outline on the box
+		love.graphics.rectangle("line",msg.x,msg.y,msg.boxW,msg.boxH)
 	end
 end
 
